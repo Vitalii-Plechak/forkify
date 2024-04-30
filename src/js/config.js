4e76945a289cac9f9347c1c33dddade4b8dd4cee
Vitalii Plechak <vitalii@ict.lv>
2024-04-30 12:47:05 -0600

    - Replaced static `API_KEY` with dynamically fetched from .env file.

--- src/js/config.js
+++ src/js/config.js
@@ -1,7 +1,7 @@
 // API https://forkify-api.herokuapp.com/v2
 
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes';
-export const API_KEY = '128fd03a-7ba3-4e1e-aea0-2ab01c259ed4';
+export const API_KEY = process.env.API_KEY;
 export const TIMEOUT_SEC = 5;
 export const RESULTS_PER_PAGE = 10
 export const BOOKMARKS_STORAGE_KEY = 'bookmarks';
